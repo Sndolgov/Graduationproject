1a5611ccd7fcfc159af088dcc58b1ea0e1155bba
Sndolgov <Dolgov.Sergey84@gmail.com>
2017-12-26 11:12:26 +0300

    populate db

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,10 +1,11 @@
-
 DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS restaurants;
-DROP TABLE IF EXISTS dishes;
-DROP TABLE IF EXISTS menu;
+DROP TABLE IF EXISTS menuconsist;
 DROP TABLE IF EXISTS voting;
+DROP TABLE IF EXISTS menu;
+DROP TABLE IF EXISTS dishes;
+DROP TABLE IF EXISTS restaurants;
+DROP TABLE IF EXISTS users;
+
 
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -26,14 +27,15 @@ CREATE SEQUENCE global_seq START 100000;*/
 
 CREATE TABLE users
 (
-  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  name             VARCHAR                  NOT NULL,
-  email            VARCHAR                  NOT NULL,
-  password         VARCHAR                  NOT NULL,
-  registered       TIMESTAMP DEFAULT now()  NOT NULL,
-  enabled          BOOL DEFAULT TRUE        NOT NULL
+  id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name       VARCHAR                 NOT NULL,
+  email      VARCHAR                 NOT NULL,
+  password   VARCHAR                 NOT NULL,
+  registered TIMESTAMP DEFAULT now() NOT NULL,
+  enabled    BOOL DEFAULT TRUE       NOT NULL
 );
-CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+CREATE UNIQUE INDEX users_unique_email_idx
+  ON users (email);
 
 CREATE TABLE user_roles
 (
@@ -45,25 +47,62 @@ CREATE TABLE user_roles
 
 CREATE TABLE restaurants
 (
-  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq') ,
-  restaurant_name  VARCHAR                  NOT NULL,
-  enabled          BOOL DEFAULT TRUE        NOT NULL
+  id      INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name    VARCHAR                  NOT NULL,
+  enabled BOOL DEFAULT TRUE        NOT NULL
 );
-CREATE UNIQUE INDEX restaurant_unique_description_idx ON restaurants (restaurant_name);
+CREATE UNIQUE INDEX restaurant_unique_name_idx
+  ON restaurants (name);
 
 
 CREATE TABLE dishes
 (
-  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq') ,
-  restaurant_id    INTEGER                  NOT NULL ,
-  description      VARCHAR                  NOT NULL ,
-  price            INTEGER                  NOT NULL DEFAULT 0 ,
-  enabled          BOOL DEFAULT TRUE        NOT NULL,
-  CONSTRAINT dishes_description_idx UNIQUE (restaurant_id, description) ,
+  id            INTEGER PRIMARY KEY                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               DEFAULT nextval(
+      'global_seq'),
+  restaurant_id INTEGER                  NOT NULL,
+  description   VARCHAR NOT NULL,
+  price         INTEGER NOT NULL                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               DEFAULT 0,
+  enabled       BOOL DEFAULT TRUE        NOT NULL,
+  CONSTRAINT dishes_description_idx UNIQUE (restaurant_id, description),
   FOREIGN KEY (restaurant_id) REFERENCES restaurants (id) ON DELETE CASCADE
 );
 
+CREATE TABLE menu
+(
+  id            INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  restaurant_id INTEGER                  NOT NULL,
+  description   VARCHAR NOT NULL,
+  date          DATE    NOT NULL,
+  enabled       BOOL DEFAULT TRUE        NOT NULL,
+  CONSTRAINT restaurant_date_idx UNIQUE (restaurant_id, date),
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants (id) ON DELETE CASCADE
+);
 
+CREATE TABLE menuconsist
+(
+  menu_id       INTEGER NOT NULL,
+  dish_id INTEGER NOT NULL,
+  restaurant_id INTEGER NOT NULL,
+  CONSTRAINT menu_dish_idx UNIQUE (menu_id, dish_id),
+  FOREIGN KEY (menu_id) REFERENCES menu (id) ON DELETE CASCADE,
+  FOREIGN KEY (dish_id) REFERENCES dishes (id) ON DELETE CASCADE,
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants (id) ON DELETE CASCADE
+
+);
+
+CREATE TABLE voting
+(
+  menu_id       INTEGER NOT NULL,
+  restaurant_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  CONSTRAINT menu_user_idx UNIQUE (menu_id, user_id),
+  FOREIGN KEY (menu_id) REFERENCES menu (id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants (id) ON DELETE CASCADE
+
+);
+
+/*
 CREATE TABLE menuconsist
 (
   id INTEGER PRIMARY KEY DEFAULT nextval('global_seq') ,
@@ -78,6 +117,7 @@ CREATE TABLE menuconsist
 );
 CREATE UNIQUE INDEX menuconsist_unique_idx ON menuconsist (date, restaurant_id, menu_id, dish_id);
 CLUSTER menuconsist USING menuconsist_unique_idx;
+*/
 
 /*
 CREATE TABLE lunchmenus
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,25 +1,82 @@
-DELETE FROM voting;
-DELETE FROM menuconsist;
-DELETE FROM dishes;
-DELETE FROM lunchmenus;
-DELETE FROM restaurants;
 DELETE FROM user_roles;
+DELETE FROM menuconsist;
+DELETE FROM voting;
+DELETE FROM menu;
+DELETE FROM dishes;
+DELETE FROM restaurants;
 DELETE FROM users;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
--- users -- id 100000
-INSERT INTO users (description, email, password)
-VALUES ('User', 'user@yandex.ru', '$2a$10$Sh0ZD2NFrzRRJJEKEWn8l.92ROEuzlVyzB9SV1AM8fdluPR0aC1ni');
 
--- users -- id 100001
-INSERT INTO users (description, email, password)
-VALUES ('Admin', 'admin@gmail.com', '$2a$10$WejOLxVuXRpOgr4IlzQJ.eT4UcukNqHlAiOVZj1P/nmc8WbpMkiju');
+INSERT INTO users (name, email, password) VALUES
+  ('User', 'user@yandex.ru', 'password'),
+  ('Admin', 'admin@gmail.com', 'admin');
 
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001),
   ('ROLE_USER', 100001);
 
+INSERT INTO restaurants (name) VALUES
+  ('Restaurant1'),
+  ('Restaurant2');
+
+INSERT INTO dishes (restaurant_id, description, price) VALUES
+  (100002, 'Soup', 200),
+  (100002, 'Meat', 300),
+  (100002, 'Juice',100),
+  (100002, 'Fish', 250),
+  (100002, 'Eggs', 110),
+  (100002, 'Pancakes', 150),
+  (100003, 'Tea', 70),
+  (100003, 'Cofee', 160),
+  (100003, 'Salad', 210),
+  (100003, 'Meat', 290),
+  (100003, 'Fish', 280);
+
+
+INSERT INTO menu (restaurant_id, description, date) VALUES
+
+    (100002, 'Menu of First restaurant', '2017-12-26'),
+    (100002, 'Menu of First restaurant', '2017-12-27'),
+    (100002, 'Menu of First restaurant', '2017-12-28'),
+    (100003, 'Menu of Second restaurant', '2017-12-26'),
+    (100003, 'Menu of Second restaurant', '2017-12-27'),
+    (100003, 'Menu of Second restaurant', '2017-12-28');
+
+
+
+
+INSERT INTO menuconsist (menu_id, restaurant_id, dish_id) VALUES
+  (100015, 100002, 100004),
+  (100015, 100002, 100005),
+  (100015, 100002, 100006),
+  (100015, 100002, 100007),
+  (100016, 100002, 100008),
+  (100016, 100002, 100009),
+  (100016, 100002, 100004),
+  (100017, 100002, 100005),
+  (100017, 100002, 100007),
+  (100018, 100003, 100010),
+  (100018, 100003, 100011),
+  (100018, 100003, 100012),
+  (100019, 100003, 100013),
+  (100019, 100003, 100014),
+  (100019, 100003, 100012),
+  (100020, 100003, 100011),
+  (100020, 100003, 100014);
+
+
+
+INSERT INTO voting (menu_id, restaurant_id  , user_id) VALUES
+  (100015, 100002, 100000),
+  (100015, 100002, 100001),
+  (100018, 100002, 100001);
+
+
+
+/*
 -- id 100002
 INSERT INTO restaurants (description)
 VALUES ('First Restaurant');
@@ -81,4 +138,4 @@ INSERT INTO users (description, email, password)
 VALUES ('User 2', 'user2@yandex.ru', '$2a$10$Sh0ZD2NFrzRRJJEKEWn8l.92ROEuzlVyzB9SV1AM8fdluPR0aC1ni');
 
 INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100016);
+  ('ROLE_USER', 100016);*/
